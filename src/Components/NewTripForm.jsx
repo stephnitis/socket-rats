70c50277ce89b6abada191bfd15d547781317abe
Stephanie Hill <stephni.hill@gmail.com>
2022-12-12 12:24:01 -0800

    accessible time picker

--- src/Components/NewTripForm.jsx
+++ src/Components/NewTripForm.jsx
@@ -1,5 +1,7 @@
 import FormControl from '@mui/material/FormControl';
 import { Input, InputLabel, FormHelperText, FormGroup } from '@mui/material';
+import 'smart-webcomponents-react/source/styles/smart.default.css';
+import { TimePicker } from 'smart-webcomponents-react/timepicker';
 
 function NewTripForm() {
 
@@ -7,15 +9,20 @@ function NewTripForm() {
   return (
     <FormGroup>
       <FormControl>
-        <InputLabel htmlFor="name-input">Name</InputLabel>
+        <InputLabel htmlFor="name-input">Full Name</InputLabel>
         <Input id="name-input" aria-describedby="input-name-text" />
-        <FormHelperText id="input-full-name-text">Input Full Name</FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="trailname-input">Name of Trail</InputLabel>
+        <Input id="trailname-input" aria-describedby="input-trail-name-text" />
       </FormControl>
       <FormControl>
         <InputLabel htmlFor="trailCoordinates-input">Trailhead Coordinates</InputLabel>
         <Input id="trailCoordinates-input" aria-describedby="input-trail-coordinates-text" />
         <FormHelperText id="input-trail-coordinates-text">Input Latitude and Longitude</FormHelperText>
-        </FormControl>
+      </FormControl>
+      <TimePicker></TimePicker>
+
     </FormGroup>
   );
 }
