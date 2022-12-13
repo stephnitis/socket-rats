01009371433ae9eb5a5e6711237d79321f61ce65
Stephanie Hill <stephni.hill@gmail.com>
2022-12-13 12:48:22 -0800

    date picker

--- src/Components/UserInfoForm.jsx
+++ src/Components/UserInfoForm.jsx
@@ -1,7 +1,24 @@
-
-import { FormControl, Input, InputLabel, FormHelperText, FormGroup} from '@mui/material';
+import React, { useState } from 'react';
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { FormControl, Input, InputLabel, FormHelperText, FormGroup, TextField } from '@mui/material';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
 function UserInfoForm() {
+
+  const [value, setValue] = useState(dayjs('2014-08-18T21:11:54'));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   let userUpdate = {
+
+  //   }
+  // }
 
   return (
     <FormGroup>
@@ -15,6 +32,16 @@ function UserInfoForm() {
         <InputLabel htmlFor="email-input">Email</InputLabel>
         <Input id="email-input" aria-describedby="input-email-text" />
       </FormControl>
+      
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DesktopDatePicker
+          label="Select Birthday"
+          inputFormat="MM/DD/YYYY"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
 
       <FormControl>
         <InputLabel htmlFor="insurance-input">Insurance Information</InputLabel>
@@ -45,7 +72,7 @@ function UserInfoForm() {
         <Input id="treatments-input" aria-describedby="input-communication-difficulties-text" />
         <FormHelperText id="treatments-input">Input Any Treatment Preferrences</FormHelperText>
       </FormControl>
-      
+
     </FormGroup>
   );
 }
--- src/Context/Settings.jsx
+++ src/Context/Settings.jsx
@@ -1,25 +1,25 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+// import axios from 'axios';
 
 export const SettingsContext = React.createContext();
-const server = process.env.REACT_APP_SERVER
+// const server = process.env.REACT_APP_SERVER
 
 const SettingsProvider = ({children}) => {
 
-  const [info, setInfo] = useState();
+  // const [info, setInfo] = useState();
 
-  const updateInfo = async (id) => {
-    try {
-      let url = `${server}/users/${id}`
-      let updatedInfo = await axios.put(url, id);
-      setInfo([...info, updatedInfo]);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  // const updateInfo = async (id) => {
+  //   try {
+  //     let url = `${server}/users/${id}`
+  //     let updatedInfo = await axios.put(url, id);      
+  //     setInfo(updatedInfo);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
 
   const values = {
-    updateInfo
+    // updateInfo
   }
 
   return (
