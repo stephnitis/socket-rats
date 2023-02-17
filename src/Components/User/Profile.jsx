3215677d553905272076dcd77a660c5cb5735bfc
Stephanie Hill <stephni.hill@gmail.com>
2023-02-16 19:03:18 -0800

    comment

--- src/Components/User/Profile.jsx
+++ src/Components/User/Profile.jsx
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { SettingsContext } from '../../Context/Settings'
 
 import {Button} from '@mui/material';
-
+// hello
 const Profile = () => {
   const { user, isAuthenticated, isLoading, getAccessTokenSilently, setUserMetadata } = useAuth0();
   const { handleShowForm } = useContext(SettingsContext);
